6bc2789dbf83b887391ff66c15abdad488518237
iagosm <iagosousa201486@gmail.com>
2025-01-22 23:32:25 -0300

    realizando cadastro através do componente

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,10 +2,64 @@
 
 namespace App\Livewire;
 
+use App\Models\Contact;
+use Illuminate\Support\Facades\Log;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+    #[Validate('required|min:3|max:50')]
+    public $name;
+    #[Validate('required|email|min:5|max:50')]
+    public $email;
+    #[Validate('required|min:5|max:20')]
+    public $phone;
+    public $error = "";
+    public $success = "";
+
+    public function newContact()
+    {
+        // validation
+        // $this->validate([
+        //     'name' => 'required|min:3|max:50',
+        //     'email' => 'required|email|min:5|max:50',
+        //     'phone' => 'required|min:5|max:20',
+        // ]);
+        $this->validate();
+
+        // temporary store in log file
+        // Log::info('Novo Contato:'. $this->name . ' - ' . $this->email . ' - ' . $this->phone);
+
+        // Store Contact in database
+        // se nome e email n existir no database
+        $result = Contact::firstOrCreate(
+            [
+                'name' => $this->name,
+                'email' => $this->email
+            ],
+            [
+                'phone' => $this->phone
+            ]
+        );
+        // clear form
+
+        // Option 1
+        // $this->name = '';
+        // $this->email = '';
+        // $this->phone = '';
+
+        // check for success or error
+        if($result->wasRecentlyCreated) {
+            // Option 2 => clear Form
+            // Clear all public properties
+            $this->reset();
+            $this->success = "Contact created successfullly;";
+        } else {
+            $this->error = "contact already exists.";
+        }
+    }
+
     public function render()
     {
         return view('livewire.form-contact');
--- resources/views/livewire/form-contact.blade.php
+++ resources/views/livewire/form-contact.blade.php
@@ -1,20 +1,38 @@
 <div class="p-5 card">
-    <form>
-        @csrf
+    <form wire:submit="newContact">
         <div class="mb-3">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name">
+            <input type="text" class="form-control" id="name" wire:model="name">
+            @error('name') 
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="email">Email</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" wire:model="email">
+            @error('email') 
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="phone">Phone</label>
-            <input type="phone" class="form-control" id="phone">
+            <input type="phone" class="form-control" id="phone" wire:model="phone">
+            @error('phone') 
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
         <div class="text-end">
             <button class="px-5 btn btn-secondary">Save</button>
         </div>
+        @if($error)
+            <div class="mt-3 text-center alert alert-danger">
+                {{ $error }}
+            </div>
+        @endif
+        @if($success)
+            <div class="mt-3 text-center alert alert-success">
+                {{ $success }}
+            </div>
+        @endif
     </form>
 </div>
